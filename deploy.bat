bd5c45e1f7ec78ba18846d07429997e0ef7ab743
Adewale Azeez <azeezadewale98gmail.com>
2019-12-14 21:59:23 +0100

    update repo-page-scripts

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,12 @@
 @echo off
 setlocal enabledelayedexpansion
 
-SET SCRIPTS_DIR=%~dp0/docs/scripts/
-SET STYLES_DIR=%~dp0/docs/styles/
+SET VERSION=%1
+if "!VERSION!"=="" (
+	SET VERSION=latest
+)
+SET SCRIPTS_DIR=%~dp0/docs/scripts/!VERSION!/
+SET STYLES_DIR=%~dp0/docs/styles/!VERSION!/
 
 IF NOT EXIST "!SCRIPTS_DIR!" ( MKDIR "!SCRIPTS_DIR!")
 IF NOT EXIST "!STYLES_DIR!" ( MKDIR "!STYLES_DIR!")
