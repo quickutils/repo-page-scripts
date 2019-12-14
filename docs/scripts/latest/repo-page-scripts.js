5a93ea2b14b8e0465dc1b3427c837860a690da71
Adewale Azeez <azeezadewale98gmail.com>
2019-12-14 22:54:04 +0100

    use xmlhttp instead of script head append to get json

--- docs/scripts/latest/repo-page-scripts.js
+++ docs/scripts/latest/repo-page-scripts.js
@@ -84,16 +84,17 @@ function setTitle(org) {
 }
 
 function getJSONP(url, success) {
-    var ud = '_' + +new Date,
-        script = document.createElement('script'),
-        head = document.getElementsByTagName('head')[0] 
-               || document.documentElement;
-    window[ud] = function(data) {
-        head.removeChild(script);
-        success && success(data);
-    };
-    script.src = url.replace('callback=?', 'callback=' + ud);
-    head.appendChild(script);
+    var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open('GET', url, true);
+	xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState == 4) {
+			if(xmlhttp.status == 200) {
+				var obj = JSON.parse(xmlhttp.responseText);
+				success(obj);
+			 }
+		}
+	};
+	xmlhttp.send(null);
 }
 
 function getOrginfoTest() {
