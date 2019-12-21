794afd6bbbfbafddcf6a4455a1a0aed52c70d0e4
Adewale Azeez <azeezadewale98@gmail.com>
2019-12-21 23:02:11 +0100

    resolve invalid release url

--- scripts/repo-page-scripts.js
+++ scripts/repo-page-scripts.js
@@ -224,7 +224,7 @@ function continueSetRepoBody(repo, readmeRaw, callback){
 		<div class="left-sidenav">			
 			<a class="left-sidenav-a" href="#" onclick="openRepoPage('Home', this)" id="defaultOpen">Home</a>`
 			
-			+ ( repo.has_downloads ? `<a class="left-sidenav-a" href="#" onclick="openReleasePage(this, '${repo.releases_url.split(\'{\')[0]}')">Releases</a>` : `` ) + 
+			+ ( repo.has_downloads ? `<a class="left-sidenav-a" href="#" onclick="openReleasePage(this, '${repo.releases_url.split('{')[0]}')">Releases</a>` : `` ) + 
 			
 			``
 			
